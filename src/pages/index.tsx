01fe1e5175d65b19852861dff2fb6e769a04aba0
Indra Zulfi Mushoddaq <indra@indiemakers.id>
2022-09-27 11:37:30 +0000

    Update 3 files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div>Hey There!</div>;
-};
+    return <div>Hey There!</div>
+}
 
-export default Home;
+export default Home
